844249f1d4d5a46bf7b47a71ad08dad80af2a1e8
LADsy8 <dube.arthur@gmail.com>
2025-03-21 15:13:29 -0400

    0.1.1: ajout de bootstrap

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
-<script setup lang="ts">
-  </script>
-
 <template>
-    <header>
-      <h1>Mon Portfolio</h1>
-      <p>Voici mes projets de programmation</p>
+    <header class="bg-primary text-white py-5">
+      <div class="container text-center">
+        <h1>Mon Portfolio</h1>
+        <p>Voici mes projets de programmation</p>
+      </div>
     </header>
   </template>
   
   <style scoped>
   </style>
+  
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -19,12 +19,19 @@ onMounted(() => {
 </script>
 
 <template>
-  <section>
-    <h2>Mes Projects Github</h2>
-    <div v-for="project in projects" :key="project.id">
-      <h3>{{ project.name }}</h3>
-      <p>{{ project.description }}</p>
-      <a :href="project.html_url" target="_blank">Voir sur Github</a>
+  <section class="container py-5">
+    <h2 class="text-center mb-4">Mes Projects Github</h2>
+    <div class="row">
+      <div class="col-md-4 mb-4" v-for="project in projects" :key="project.id">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{{ project.name }}</h5>
+            <p class="card-text" v-if="project.description">{{ project.description }}</p>
+            <p class="card-text" v-else>Aucune description disponible</p>
+            <a :href="project.html_url" class="btn btn-primary" target="_blank">Voir sur Github</a>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
